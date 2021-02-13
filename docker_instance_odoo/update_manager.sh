726790b390551a8a8862bffd6039ad3a4045eef3
Mauricio Sanchez <mauricio@vauxoo.com>
2021-02-13 18:21:44 -0400

    dummy

--- docker_instance_odoo/update_manager.sh
+++ docker_instance_odoo/update_manager.sh
@@ -3,7 +3,7 @@
 
 dbname="produccion"
 db_host=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yoytec_server_db)
-fileyml='/home/odoo/docker_instance_odoo/docker_instance_odoo/docker-compose.yml'
 workerdir='/home/odoo/docker_instance_odoo/docker_instance_odoo'
+fileyml='${workerdir}/docker-compose.yml'
 
 /usr/bin/manager_instance.py -f ${fileyml} -s ${db_host} -d ${dbname} -w ${workerdir} $@
